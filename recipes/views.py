9f7ed28e7e58929971c7cf50ac9634ed58e8f527
chrys48116 <chrys481@gmail.com>
2023-03-23 17:17:47 -0300

    Pagina seguinte adicionada

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from utils.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes':[make_recipe() for _ in range(10)],
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
 
